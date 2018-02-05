5d8e8a5d7a3a4062f9100f3130f6fcf1716853df
Daniel Ostrovsky <danielo@stuccomedia.com>
2018-02-05 16:38:27 +0200

    gh-pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  weatherApiUrl: 'http://api.openweathermap.org/data/2.5/',
+  weatherApiUrl: 'https://api.openweathermap.org/data/2.5/',
   weatherAppId: '68566bb8e5ce2b7e65197cf4cb2722bf',
   units: 'metric'
 };
